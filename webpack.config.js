6e398757d4a9697c9f0c823b3320f4d530029972
BharathiManisekar <bharathi.manisekarme@gmail.com>
2022-06-06 13:39:18 +0530

    remove class-properties

--- webpack.config.js
+++ webpack.config.js
@@ -85,7 +85,6 @@ module.exports = {
               "@babel/proposal-class-properties",
               "@babel/plugin-transform-runtime",
               "@babel/plugin-transform-react-jsx",
-              ["@babel/plugin-proposal-class-properties", { loose: true }],
             ],
           },
         },
