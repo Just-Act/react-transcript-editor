3498798904dcf4d4de088f960fa9bdc75e4584b4
Maheswari <maheswari.j@justact.co.in>
2022-12-16 11:28:44 +0530

    build

--- __mocks__/styleMock.js
+++
@@ -1,5 +0,0 @@
-// __mocks__/styleMock.js
-// mocking css files and other media for jest when used with react import
-// see also package.json under jest -> moduleNameMapper
-// https://jestjs.io/docs/en/webpack.html
-module.exports = {};
